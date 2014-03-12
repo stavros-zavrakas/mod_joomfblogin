784747e150af9bbef3ac73dc1b2c30c724480189
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2014-03-12 20:46:31 +0100

    first version of module

---
+++ tmpl/default.php
@@ -0,0 +1,21 @@
+<?php // no direct access
+	defined( '_JEXEC' ) or die( 'Restricted access' ); 
+?>
+
+<div class="<?php echo $params->get('moduleclass_sfx'); ?>">
+	<!-- The fields that exists will be displayed only if are not empty -->
+	<div> <?php echo $params->get('pretext'); ?> </div>
+
+	<?php 
+		if(!empty($fb_app_id)) {
+			echo "'" . $fb_app_id . "'"; 
+			$document = JFactory::getDocument();
+
+			$document->addScript(JURI::root() . 'media/mod_helloworld/js/functions.js');
+		} else {
+			echo "Critical Error!";
+		}
+	?>
+
+	<div> <?php echo $params->get('posttext'); ?> </div>
+</div>
